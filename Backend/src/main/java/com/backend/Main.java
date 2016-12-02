c871a6bebf589c9c3f8a0ca31757b884ec4ffbef
Felix Hambrecht <fhambrec@mail.uni-mannheim.de>
2016-12-02 20:24:38 +0100

    cleared structured

--- Backend/src/main/java/com/backend/Main.java
+++ Backend/src/main/java/com/backend/Main.java
@@ -1,8 +1,15 @@
 package com.backend;
 
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlineParameter;
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlinePegelinformation;
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlineWebservicePortType;
+import de.wsv.pegelonline.webservices.version2_3.api.PegelonlineWebservice_Impl;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.rmi.RemoteException;
 
 /**
  * Created by Felix on 02.12.2016.
@@ -10,9 +17,38 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * Project: DataRun-WaterLevels
  */
 @SpringBootApplication
-@EnableScheduling
 public class Main {
     public static void main(String[] args){
         SpringApplication.run(Main.class, args);
+
+        PegelonlineWebservicePortType port =
+                new PegelonlineWebservice_Impl().getPegelonlineWebservicePort();
+
+        //Abfrage getParameterList: Alle in Pegelonline Webservice-Aktuell enthaltenden Parameter
+        //werden zurückgeliefert
+        //Bei PegelonlineParameter handelt es sich um einen custom-type aus Pegelonline Webservice-Aktuell
+        try{
+            PegelonlineParameter[] par = new PegelonlineParameter[0];
+            par = port.getParameterList();
+
+            //Ausgabe der Parameternamer
+
+                PegelonlinePegelinformation[] pi =
+            port.getPegelinformationen(null, null, new String[] {"EITZE"});
+
+                //Ausgabe der aktuellen Messung der Messstelle TRUNSTADT für alle Parameter
+                for (int x = 0; x < pi.length; x++) {
+                    System.out.println(
+                            "Letzte Messung vom " +
+                                    pi[x].getPegelonlineAktuelleMessung().getZeitpunkt().getTime() +
+                                    " mit dem Wert " + pi[x].getPegelonlineAktuelleMessung().getMesswert() +
+                                    " für den Parameter " + pi[x].getPegelonlineParameter().getName());
+                    System.out.println();
+                }
+
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Backend/src/main/java/com/backend/serve/ServeController.java
+++ Backend/src/main/java/com/backend/serve/ServeController.java
@@ -1,7 +1,36 @@
 package com.backend.serve;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Created by masc on 02.12.2016.
  */
+@RestController
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ServeController {
+
+    @RequestMapping("/waterlevels")
+    public double[] waterlevels() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Object object = restTemplate.getForObject("http://www.pegelonline.wsv.de/webservices/rest-api/v2/stations.json", Object.class);
+        System.out.println(object.toString());
+
+        //ObjectMapper mapper = new ObjectMapper();
+
+        //Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+
+
+        return null;
+    }
+
+
 }
